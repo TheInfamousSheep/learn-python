00a04981e51860de544ad9f3492b27b8b292d2c1
TheInfamousSheep <80496452+TheInfamousSheep@users.noreply.github.com>
2021-03-11 20:45:06 -0500

    Update magic8.py
    now it makes sure that you put in a question and if you did not put a name it still works

--- 2-control-flow/magic-8-ball/magic8.py
+++ 2-control-flow/magic-8-ball/magic8.py
@@ -1,32 +1,35 @@
-import random
+name = ""
 
-name = "Joe"
-question = "Will I win the lottery?"
+question = ""
 answer = ""
 
-random_number = random.randint(1, 9)
-# print(random_number)
+import random
+random_Number = random.randint(1,9)
 
-if random_number == 1:
-  answer = "Yes - definitely"
-elif random_number == 2:
-  answer = "It is decidedly so"
-elif random_number == 3:
-  answer = "Without a doubt"
-elif random_number == 4:
-  answer = "Reply hazy, try again"
-elif random_number == 5:
-  answer = "Ask again later"
-elif random_number == 6:
-  answer = "Better not tell you now"
-elif random_number == 7:
-  answer = "My sources say no"
-elif random_number == 8:
-  answer = "Outlook not so good"
-elif random_number == 9:
-  answer = "Very doubtful"
+if question == "" or question == " ":
+  print("please input a question")
 else:
-  answer = "Error"
-  
-print(name + " asks: " + question)
-print("Magic 8 Ball's answer: " + answer)
+  if name == "" or name == " ":
+    print("you ask:  " + "\"" + question + "\"")
+  else:
+    print(name,"asks:  " + "\"" + question + "\"")
+
+  if random_Number == 1:
+    answer = "Yes definetly"
+  elif random_Number == 2:
+    answer="It is decidedly so"
+  elif random_Number == 3:
+    answer="Without a doubt"
+  elif random_Number == 4:
+    answer="Reply hazy, try again"
+  elif random_Number == 5:
+    answer="Ask again later"
+  elif random_Number == 6:
+    answer="Better not tell you now"
+  elif random_Number == 7:
+    answer="My sources say no"
+  elif random_Number == 8:
+    answer="Outlook not so good"
+  else:
+    answer="Very doubtful"
+  print("my sources say that:",answer)
